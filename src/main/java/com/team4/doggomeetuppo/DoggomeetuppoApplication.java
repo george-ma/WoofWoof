1db1511f8b0c4a6a47e295664bf8ba96458e2e52
matthew-jay-wong <matthew.jay.wong@gmail.com>
2018-10-10 23:50:37 -0400

    first commit

---
+++ src/main/java/com/team4/doggomeetuppo/DoggomeetuppoApplication.java
@@ -0,0 +1,12 @@
+package com.team4.doggomeetuppo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoggomeetuppoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoggomeetuppoApplication.class, args);
+	}
+}
