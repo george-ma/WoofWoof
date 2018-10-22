2d66d74a5d3251dd65a3625ff91ab225e349ef8e
Tiger <tigeryu7@gmail.com>
2018-10-22 11:35:44 -0400

    added initial EventController.java

--- src/main/java/com/team4/doggomeetuppo/Service/EventRepository.java
+++ src/main/java/com/team4/doggomeetuppo/Service/EventRepository.java
@@ -1,6 +1,5 @@
 package com.team4.doggomeetuppo.Service;
 
-import com.team4.doggomeetuppo.Model.User;
 import com.team4.doggomeetuppo.Model.Event;
 import org.springframework.stereotype.Repository;
 
@@ -8,7 +7,7 @@ import java.util.List;
 
 @Repository
 public interface EventRepository {
-    public User findByEventName(String eventName);
+    public Event findByEventName(String eventName);
     public List<Event> getAllEvents();
     public void saveEvent(Event event);
     public void addEvent(Event event);
