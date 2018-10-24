d43b20074cbb4a9d4f84eedbf37ebf01dd6fc13f
Matthew Wong <matthew.jay.wong@gmail.com>
2018-10-24 01:07:23 -0400

    Got map and current location to work

--- frontend/src/pages/about/about.ts
+++ frontend/src/pages/about/about.ts
@@ -1,14 +1,37 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { Geolocation } from '@ionic-native/geolocation';
+import { } from 'google-maps';
+
 @Component({
   selector: 'page-about',
   templateUrl: 'about.html'
 })
 export class AboutPage {
 
-  constructor(public navCtrl: NavController) {
+  map: any;
 
+  constructor(
+    public navCtrl: NavController,
+    private geolocation: Geolocation
+  ) {
+
+  }
+
+  ionViewDidEnter() {
+    this.geolocation.getCurrentPosition().then((resp) => {
+      this.map = new google.maps.Map(
+        document.getElementById('map'),
+        {
+          center: {
+            lat: resp.coords.latitude,
+            lng: resp.coords.longitude
+          },
+          zoom: 15
+        }
+      );
+    });
   }
 
 }
