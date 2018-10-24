d29e6849f091a64775848b98e3285c97359af775
Matthew Wong <matthew.jay.wong@gmail.com>
2018-10-24 02:11:18 -0400

    Show all parks within 1km radius

--- frontend/src/pages/about/about.ts
+++ frontend/src/pages/about/about.ts
@@ -11,27 +11,64 @@ import { } from 'google-maps';
 export class AboutPage {
 
   map: any;
+  currentMarker: any;
+  allDogParks: google.maps.Marker[];
+
+  GooglePlaces: any;
 
   constructor(
     public navCtrl: NavController,
     private geolocation: Geolocation
   ) {
-
+    this.allDogParks = [];
   }
 
   ionViewDidEnter() {
+    this.initMap();
+  }
+
+  initMap() {
     this.geolocation.getCurrentPosition().then((resp) => {
+      const currentPosition = {
+        lat: resp.coords.latitude,
+        lng: resp.coords.longitude
+      }
       this.map = new google.maps.Map(
         document.getElementById('map'),
         {
-          center: {
-            lat: resp.coords.latitude,
-            lng: resp.coords.longitude
-          },
+          center: currentPosition,
           zoom: 15
         }
       );
+      this.currentMarker = new google.maps.Marker({
+        position: currentPosition,
+        map: this.map,
+        title: 'I am here!'
+      });
+      const placeService = new google.maps.places.PlacesService(this.map);
+      placeService.nearbySearch({
+        location: currentPosition,
+        radius: 1000,
+        type: 'park'
+      }, (results, status) => {
+        if (status === google.maps.places.PlacesServiceStatus.OK) {
+          results.forEach((place) => {
+            this.allDogParks.push(new google.maps.Marker({
+              map: this.map,
+              position: place.geometry.location,
+            }));
+          });
+        }
+      });
     });
   }
+
+  createMarker(place) {
+
+  }
+
+  // loadDogParks() {
+  //   this.GooglePlaces.
+  // }
 
 }
