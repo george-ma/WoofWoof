a785b2300077212e1d1b142c67c2c24744b55b7e
grace-jyz <grace.zou319@gmail.com>
2018-10-29 01:27:38 -0400

    added search bar in events

--- frontend/src/pages/events/events.ts
+++ frontend/src/pages/events/events.ts
@@ -6,9 +6,32 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'events.html'
 })
 export class EventsPage {
+  parkEvents;
 
   constructor(public navCtrl: NavController) {
+    this.initializeEvents();
 
+  }
+
+  initializeEvents() {
+    this.parkEvents = [
+      'Doggo 2nd Birthday Party',
+      'Halloween Costume Party',
+      'Obstacle Race'
+    ];
+  }
+
+  searchEvents(ev) {
+    this.initializeEvents();
+
+    var search = ev.target.value;
+
+    // if the value is an empty string don't filter the items
+    if (search && search.trim() != '') {
+      this.parkEvents = this.parkEvents.filter((event) => {
+        return (event.toLowerCase().indexOf(search.toLowerCase()) > -1);
+      })
+    }
   }
 
 }
