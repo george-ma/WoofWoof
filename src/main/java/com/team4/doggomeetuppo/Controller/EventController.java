e010bf891255ebc560de88e566c424ccc37a71ed
Matthew Wong <matthew.jay.wong@gmail.com>
2018-11-29 11:01:31 -0500

    enable cords

--- src/main/java/com/team4/doggomeetuppo/Controller/EventController.java
+++ src/main/java/com/team4/doggomeetuppo/Controller/EventController.java
@@ -12,7 +12,6 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:8100")
 @RestController
 @RequestMapping(value = "/api/event")
 public class EventController {
@@ -23,6 +22,7 @@ public class EventController {
     @Autowired
     private ImageRepository imageRepository;
 
+    @CrossOrigin
     @RequestMapping(value = "/getEventByName", method = RequestMethod.GET)
     public Event getEventByEventName(
             @RequestParam(value = "eventName") String eventName
@@ -30,27 +30,32 @@ public class EventController {
         return eventRepository.findByEventName(eventName);
     }
 
+    @CrossOrigin
     @RequestMapping(value = "/geocode", method = RequestMethod.POST)
     public List<Event> getEventsByPlaceName(@RequestBody String geocode) {
         return eventRepository.findByPlaceName(geocode);
     }
 
+    @CrossOrigin
     @RequestMapping(value = "/allEvents", method = RequestMethod.GET)
     public List<Event> getAllEvents() {
         return eventRepository.getAllEvents();
     }
 
+    @CrossOrigin
     @RequestMapping(value = "/save", method = RequestMethod.POST)
     public void saveEvent(@RequestBody Event event) {
         eventRepository.saveEvent(event);
     }
 
+    @CrossOrigin
     @RequestMapping(value = "/add", method = RequestMethod.PUT)
     public void addEvent(@RequestBody Event event) {
         event.setStatus(Event.status.PLANNED);
         eventRepository.addEvent(event);
     }
 
+    @CrossOrigin
     @RequestMapping(value = "/saveEventPic", method = RequestMethod.PUT)
     public ResponseEntity addEventPic(
             @RequestParam(value = "eventPic") MultipartFile eventPic,
@@ -64,6 +69,7 @@ public class EventController {
         }
     }
 
+    @CrossOrigin
     @PostMapping(value = "/getEventPic", produces = MediaType.IMAGE_JPEG_VALUE)
     public @ResponseBody
     byte[] getDogPic(
@@ -73,6 +79,7 @@ public class EventController {
         return imageRepository.getEventPic(parkName, eventName);
     }
 
+    @CrossOrigin
     @PostMapping(value = "/attendEvent")
     public ResponseEntity attendEvent(
             @RequestParam(value = "geocode") String geocode,
@@ -85,6 +92,7 @@ public class EventController {
         return new ResponseEntity(HttpStatus.OK);
     }
 
+    @CrossOrigin
     @PostMapping(value = "/removeAttend")
     public ResponseEntity removeAttend(
             @RequestParam(value = "geocode") String geocode,
@@ -97,6 +105,7 @@ public class EventController {
         return new ResponseEntity(HttpStatus.OK);
     }
 
+    @CrossOrigin
     @PostMapping(value = "/removeAttend")
     public @ResponseBody boolean isGoing(
             @RequestParam(value = "geocode") String geocode,
--- src/main/java/com/team4/doggomeetuppo/Controller/UserController.java
+++ src/main/java/com/team4/doggomeetuppo/Controller/UserController.java
@@ -13,7 +13,6 @@ import org.springframework.web.multipart.MultipartFile;
 import java.util.List;
 import java.util.stream.Collectors;
 
-@CrossOrigin
 @RestController
 @RequestMapping(value = "/api/user")
 public class UserController {
@@ -29,16 +28,19 @@ public class UserController {
         return userRepository.findByUserName(userName);
     }
 
+    @CrossOrigin
     @GetMapping(value = "/allUsers")
     public List<User> getAllUsers() {
         return userRepository.getAllUsers();
     }
 
+    @CrossOrigin
     @PostMapping(value = "/save")
     public void saveUser(@RequestBody User user) {
         userRepository.saveUser(user);
     }
 
+    @CrossOrigin
     @PostMapping(value = "/verifyLogin")
     public boolean verifyLogin(
             @RequestParam(value = "userName") String userName,
@@ -49,6 +51,7 @@ public class UserController {
                 .anyMatch(user -> user.getUserName().equals(userName) && user.getPassword().equals(password));
     }
 
+    @CrossOrigin
     @PostMapping(value = "/saveProfilePic")
     public ResponseEntity saveProfilePic(
             @RequestParam(value = "profilePic") MultipartFile profilePic,
@@ -61,11 +64,13 @@ public class UserController {
         }
     }
 
+    @CrossOrigin
     @GetMapping(value = "/getProfilePic/{username}", produces = MediaType.IMAGE_JPEG_VALUE)
     public @ResponseBody byte[] getProfilePic(@PathVariable String username) {
         return imageRepository.getProfilePic(username);
     }
 
+    @CrossOrigin
     @PostMapping(value = "/saveDogPic", consumes = "multipart/form-data")
     public ResponseEntity saveDogPic(
         @RequestParam(value = "profilePic") MultipartFile profilePic,
@@ -81,17 +86,20 @@ public class UserController {
         }
     }
 
+    @CrossOrigin
     @GetMapping(value = "/getDogPic/{username}/{dogname}", produces = MediaType.IMAGE_JPEG_VALUE)
     public @ResponseBody byte[] getDogPic(@PathVariable("username") String username,@PathVariable("dogname") String dogname) {
         return imageRepository.getDogPic(username,dogname);
     }
 
+    @CrossOrigin
     @GetMapping(value = "/getLocation/{username}")
     public @ResponseBody String getLocation(@PathVariable("username") String userName){
          User user = userRepository.findByUserName(userName);
          return user.getGeocode();
     }
 
+    @CrossOrigin
     @PostMapping(value = "/checkIn/{username}")
     public void checkIn(@PathVariable("username") String userName, @RequestParam(value = "geocode") String geocode){
         User user = userRepository.findByUserName(userName);
@@ -99,12 +107,9 @@ public class UserController {
         userRepository.saveUser(user);
     }
 
+    @CrossOrigin
     @PostMapping(value = "/allUsersAtPark")
     public @ResponseBody List<String> getAllUsersAtPark(@RequestParam(value = "geocode") String geocode) {
         return userRepository.findUserByLocation(geocode).stream().map(User::getUserName).collect(Collectors.toList());
     }
-
-//    <img [src]="'data:image/JPEG;base64,' + result.arrayofbytes" />
-    // display images
-
 }
